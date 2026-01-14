20b751cbb82448c87ccbc2eda51f47dc349aebe2
Gerald Andal <gerald.andal@unexus.pg>
2026-01-14 09:39:46 +0800

    Add project files excluding main.js

--- src/main.js
+++ src/main.js
@@ -1,18 +1,30 @@
+/* eslint-disable no-unused-vars */
+
 import Vue from 'vue'
 import Vuex, { mapState } from 'vuex'
 import moment from 'moment'
 import { upperFirst, camelCase } from 'lodash'
 import ToggleButton from 'vue-js-toggle-button'
 
+/* ✅ equivalent ng: import './assets/app.css' */
+import './assets/app.css'
+
+/* ✅ equivalent ng: import App from './App.vue' */
 import PrintApp from './PrintApp.vue'
 import PrintLayout from './PrintLayout.vue'
 
-// Vue Configuration
-Vue.config.productionTip = true
+/*
+|--------------------------------------------------------------------------
+| VUE CONFIG
+|--------------------------------------------------------------------------
+*/
+Vue.config.productionTip = false
 
-// Autoload components and templates
-// see https://vuejs.org/v2/guide/components-registration.html#Automatic-Global-Registration-of-Base-Components
-
+/*
+|--------------------------------------------------------------------------
+| AUTOLOAD COMPONENTS
+|--------------------------------------------------------------------------
+*/
 const requireComponent = require.context(
   './components',
   false,
@@ -23,18 +35,21 @@ requireComponent.keys().forEach(fileName => {
   const componentConfig = requireComponent(fileName)
   const componentName = upperFirst(
     camelCase(
-      fileName
-        .split('/')
-        .pop()
-        .replace(/\.\w+$/, '')
+      fileName.split('/').pop().replace(/\.\w+$/, '')
     )
   )
+
   Vue.component(
     componentName,
     componentConfig.default || componentConfig
   )
 })
 
+/*
+|--------------------------------------------------------------------------
+| AUTOLOAD TEMPLATES
+|--------------------------------------------------------------------------
+*/
 const requireTemplate = require.context(
   './templates',
   false,
@@ -45,176 +60,124 @@ requireTemplate.keys().forEach(fileName => {
   const templateConfig = requireTemplate(fileName)
   const templateName = upperFirst(
     camelCase(
-      fileName
-        .split('/')
-        .pop()
-        .replace(/\.\w+$/, '')
+      fileName.split('/').pop().replace(/\.\w+$/, '')
     )
   )
+
   Vue.component(
     templateName,
     templateConfig.default || templateConfig
   )
 })
 
-// Register default print layout
-Vue.component('default-print-layout', PrintLayout);
+/*
+|--------------------------------------------------------------------------
+| DEFAULT PRINT LAYOUT
+|--------------------------------------------------------------------------
+*/
+Vue.component('default-print-layout', PrintLayout)
 
-// Setup data stores
+/*
+|--------------------------------------------------------------------------
+| VUEX STORE
+|--------------------------------------------------------------------------
+*/
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
     debugMode: false,
     printOptions: {
-      template: null,
-      paperSize: null,
+      template: 'DoctorLetterhead', // ✅ default template
+      paperSize: 'A5',
     },
     formData: {},
   },
-  mutations: {
-    setPaperSize: (state, paperSize) => {
-      state.printOptions = {
-        ...state.printOptions,
-        paperSize,
-      }
 
-      document.body.className = paperSize
+  mutations: {
+    setPaperSize(state, paperSize) {
+  state.printOptions.paperSize = paperSize
+  document.body.className = paperSize   
+},
+
+    setPrintTemplate(state, template) {
+      state.printOptions.template = template
     },
-    setPrintTemplate: (state, template) => {
-      state.printOptions = {
-        ...state.printOptions,
-        template,
-      }
+
+    setFormData(state, formData) {
+      state.formData = { ...formData }
     },
-    setFormData: (state, formData) => {
-      state.formData = {
-        ...formData,
-      }
+
+    setDebugMode(state, debugMode) {
+      state.debugMode = debugMode
     },
-    setDebugMode: (state, debugMode) => {
-      state = {
-        ...state,
-        debugMode
-      }
-    },
-  }
+  },
 })
 
+/*
+|--------------------------------------------------------------------------
+| GLOBAL MIXIN
+|--------------------------------------------------------------------------
+*/
 Vue.mixin({
   data() {
     return {
       dynamicFields: {},
     }
   },
+
   computed: mapState([
     'debugMode',
     'printOptions',
-    'paperSize',
     'formData',
   ]),
+
   methods: {
-    registerEditableFields: function (fields) {
-      this.dynamicFields = fields;
-    }
-  }
+    registerEditableFields(fields) {
+      this.dynamicFields = fields
+    },
+  },
 })
 
-// Setup Addons
+/*
+|--------------------------------------------------------------------------
+| PLUGINS & FILTERS
+|--------------------------------------------------------------------------
+*/
 Vue.use(ToggleButton)
 
-Vue.filter('formatDate', function (value, dateFormat) {
-  if (value) {
-    return moment(String(value)).format(dateFormat)
-  }
+Vue.filter('formatDate', (value, format) => {
+  if (!value) return ''
+  return moment(String(value)).format(format)
 })
 
-// Initialize the Vue application
+/*
+|--------------------------------------------------------------------------
+| APP INIT  ✅ (ito ang katumbas ng render: h => h(App))
+|--------------------------------------------------------------------------
+*/
 new Vue({
-  render: h => h(PrintApp),
   store,
+  render: h => h(PrintApp),
 }).$mount('#app')
 
-// Functions for updating content from external source
-window.setPaperSize = (paperSize) => {
-  store.commit('setPaperSize', paperSize);
+/*
+|--------------------------------------------------------------------------
+| EXTERNAL API (FOR IFRAME / PARENT APP)
+|--------------------------------------------------------------------------
+*/
+window.setPaperSize = paperSize => {
+  store.commit('setPaperSize', paperSize)
 }
 
-window.setPrintTemplate = (template) => {
-  store.commit('setPrintTemplate', template);
+window.setPrintTemplate = template => {
+  store.commit('setPrintTemplate', template)
 }
 
-window.setFormData = (formData) => {
-  store.commit('setFormData', formData);
+window.setFormData = formData => {
+  store.commit('setFormData', formData)
 }
 
-window.setDebugMode = (debugMode) => {
-  store.commit('setDebugMode', debugMode);
+window.setDebugMode = debugMode => {
+  store.commit('setDebugMode', debugMode)
 }
-
-// The following lines are for testing
-// Faker is used to generate test content
-
-// import faker from 'faker';
-
-// const $_GET = {};
-// if (document.location.toString().indexOf('?') !== -1) {
-//   const query = document.location
-//     .toString()
-//     .replace(/^.*?\?/, '')
-//     .replace(/#.*$/, '')
-//     .split('&');
-
-//   for (let i = 0, l = query.length; i < l; i++) {
-//     const aux = decodeURIComponent(query[i]).split('=');
-//     $_GET[aux[0]] = aux[1];
-//   }
-// }
-
-// window.setPrintTemplate($_GET['template'])
-// window.setPaperSize('A5')
-
-// const sampleForm = {
-//   doctor: {
-//     first_name: faker.fake("{{name.lastName}}"),
-//     last_name: faker.fake("{{name.firstName}}"),
-//     license_number: '20200119',
-//     specialization: 'Pediatrics',
-//     clinics: [],
-//   },
-//   patient: {
-//     first_name: faker.fake("{{name.lastName}}"),
-//     last_name: faker.fake("{{name.firstName}}"),
-//     birth_date: faker.date.past().toDateString(),
-//   },
-//   prescription: {
-//     prescription_code: faker.fake("{{random.number}}"),
-//     secure_payload: {
-//       home_instructions: faker.fake("{{lorem.sentence}}"),
-//       medications: [],
-//     }
-//   },
-// };
-
-// // Generate medicine data
-// for(let i = 0; i < 20; i++) {
-//   sampleForm.prescription.secure_payload.medications.push({
-//     generic_name: faker.fake("{{commerce.productMaterial}}"),
-//     brand_name: faker.fake("{{commerce.productName}}"),
-//     formulation: `${(i+1)*100} mg`,
-//     sig: faker.fake("{{lorem.sentence}}"),
-//   })
-// }
-
-// for (let i = 0; i < 2; i++) {
-//   sampleForm.doctor.clinics.push({
-//     name: faker.fake("{{company.companyName}}"),
-//     address: faker.fake("{{address.streetAddress}}"),
-//     locality: faker.fake("{{address.city}}"),
-//     contact_number: faker.fake("{{phone.phoneNumber}}"),
-//     appointments_hotline: faker.fake("{{phone.phoneNumber}}"),
-//   })
-// }
-
-// window.setFormData(sampleForm)
-// window.setDebugMode(true)
